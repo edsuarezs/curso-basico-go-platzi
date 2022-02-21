838a431dca7e8ca833d74a946d473206d5cf6546
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-21 08:22:07 -0500

    feat: fmt package class

--- src/main.go
+++ src/main.go
@@ -4,61 +4,25 @@ import "fmt"
 
 func main() {
 
-	// Calculo area de un cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
+	//Declaración de variables
+	helloMessage := "Hello "
+	worldMessage := "World"
 
-	fmt.Println("área cuadrado: ", areaCuadrado)
+	//Println
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
 
-	x := 10
-	y := 50
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos) //Mientras lo sepa una buena practica es agregar el tipo de datos
 
-	// Suma
-	result := x + y
-	fmt.Println("Suma: ", result)
+	// Sprintf
+	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
+	fmt.Println(message)
 
-	// Resta
-	result = x - y
-	fmt.Println("Resta: ", result)
-
-	// Multiplicación
-	result = x * y
-	fmt.Println("Multiplicación: ", result)
-
-	// Division
-	result = x / y
-	fmt.Println("Division: ", result)
-
-	// Modulo
-	result = x % y
-	fmt.Println("Modulo: ", result)
-
-	// Incremental
-	x++
-	fmt.Println("Incremental: ", x)
-
-	// Decremental
-	x--
-	fmt.Println("Decremental", x)
-
-	// Reto: Cálcular el area del rectángulo, trapecio y de un circulo
-
-	const pi float64 = 3.14159
-	var radio float64
-	var circunferencia float64 = 19
-	base := x
-	altura := y
-	// área rectángulo
-	result = base * altura
-	fmt.Println("El área del rectángulo es: ", result)
-	// área del trapecio
-	base2 := base + x
-	result = ((base2 + base) * altura) / 2
-	fmt.Println("El área del trapecio es: ", result)
-	// área del circulo
-	radio = circunferencia / 2
-	result2 := float64(result)
-	result2 = pi * (radio * radio)
-	fmt.Println("El área del circulo es: ", result2)
-
+	//Tipo de datos
+	fmt.Printf("helloMessage: %T \n", helloMessage)
+	fmt.Printf("cursos: %T \n", cursos)
 }
