12a338cce85782e2704db34eaaf636d416bab890
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-22 08:05:47 -0500

    feat: Switch operator.

--- src/main.go
+++ src/main.go
@@ -4,22 +4,21 @@ import "fmt"
 
 func main() {
 
-	valor1 := 1
-	valor2 := 2
-
-	if valor1 == 1 {
-		fmt.Println("Es 1")
-	} else {
-		fmt.Println("No es 1")
+	switch modulo := 5 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
 	}
 
-	// with and
-	if valor1 == 1 && valor2 == 2 {
-		fmt.Println("Es verdad")
-	}
-
-	// with or
-	if valor1 == 0 || valor2 == 2 {
-		fmt.Println("Es verdad")
+	// sin condicion
+	value := 50
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
 	}
 }
