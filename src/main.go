3303d00f04841966ec7149b358bf7b2f2fb6c43b
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-22 08:41:40 -0500

    feat: Use of maps with key value.

--- src/main.go
+++ src/main.go
@@ -1,38 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindromo(text string) {
-	minus := strings.ToLower(text)
-	var textReverse string
-	for i := len(minus) - 1; i >= 0; i-- {
-		textReverse += string(minus[i])
-	}
-
-	if minus == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es un palindromo")
-	}
-}
+import "fmt"
 
 func main() {
-	slice := []string{"hola", "que", "hace"}
-	// este for imprime el indice del slice y el valor del indice del slice
-	for i, valor := range slice {
+	m := make(map[string]int)
+
+	m["Jose"] = 14
+	m["Pepito"] = 20
+
+	fmt.Println(m)
+
+	// Recorrer map
+	for i, valor := range m {
 		fmt.Println(i, valor)
 	}
-	// este for imprime solo el valor del slice
-	for _, valor := range slice {
-		fmt.Println(valor)
-	}
-	// este for imprime solo el indice del slice
-	for i := range slice {
-		fmt.Println(i)
-	}
 
-	isPalindromo("Ama")
+	// Encontrar un valor
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 }
