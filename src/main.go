182c12c28be7f4ebe6ca4a9fca7bb0abbb4970f1
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-22 08:33:50 -0500

    feat: slice traversal with range

--- src/main.go
+++ src/main.go
@@ -1,30 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	minus := strings.ToLower(text)
+	var textReverse string
+	for i := len(minus) - 1; i >= 0; i-- {
+		textReverse += string(minus[i])
+	}
+
+	if minus == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es un palindromo")
+	}
+}
 
 func main() {
-	// Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	slice := []string{"hola", "que", "hace"}
+	// este for imprime el indice del slice y el valor del indice del slice
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
+	// este for imprime solo el valor del slice
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
+	// este for imprime solo el indice del slice
+	for i := range slice {
+		fmt.Println(i)
+	}
 
-	// Slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Métodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Append nueva list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	isPalindromo("Ama")
 }
