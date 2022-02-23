51cc13907c4fbbb8c2c788fae7cdd514f3e379a2
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-23 18:36:25 -0500

    feat: Go routines

--- src/main.go
+++ src/main.go
@@ -1,40 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type cuadrado struct {
-	base float64
+func say(text string, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	fmt.Println(text)
 }
-
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-type figuras2D interface {
-	area() float64
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-func calcular(f figuras2D) {
-	fmt.Println("Area: ", f.area())
-}
-
 func main() {
-	myCuadrado := cuadrado{base: 2}
-	myRectangulo := rectangulo{base: 2, altura: 4}
+	var wg sync.WaitGroup
 
-	calcular(myCuadrado)
-	calcular(myRectangulo)
+	fmt.Println("Hello")
+	wg.Add(1)
 
-	// Lista interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface...)
+	go say("World", &wg)
+
+	wg.Wait()
 }
