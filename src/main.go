2c2c128697cf291dd5d1c641b696197f3541bd28
Edward Suarez <edward.suarez@mercadolibre.com.co>
2022-02-23 14:04:21 -0500

    feat: Stringers class

--- src/main.go
+++ src/main.go
@@ -4,39 +4,17 @@ import "fmt"
 
 type pc struct {
 	ram   int
-	disk  int
 	brand string
+	disk  int
 }
 
-func (myPC pc) ping() {
-	fmt.Println(myPC.brand, "Pong")
-}
-
-func (myPC *pc) duplicateRAM() {
-	myPC.ram = myPC.ram * 2
+func (myPC pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
 }
 
 func main() {
-	a := 50
-	b := &a
 
-	fmt.Println(b)
-	fmt.Println(*b)
-
-	*b = 100
-	fmt.Println(a)
-
-	myPC := pc{ram: 16, disk: 200, brand: "msi"}
-
+	myPC := pc{ram: 16, brand: "", disk: 100}
 	fmt.Println(myPC)
 
-	myPC.ping()
-
-	fmt.Println(myPC)
-	myPC.duplicateRAM()
-
-	fmt.Println(myPC)
-	myPC.duplicateRAM()
-
-	fmt.Println(myPC)
 }
